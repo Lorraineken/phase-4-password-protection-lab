9a83ddabb097217f6cb8b255cba7c1edf245f45c
Lorraineken <lorrainekupa@gmail.com>
2023-03-24 16:07:26 +0300

    created Sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
+   
 
   def create 
-    
+    user = User.create!(user_params)
+    session[:user_id]= user.id
+    render json: user
   end
 
   def show 
+   user = User.find(session[:user_id])
+    if user.&authenticate(params[:password])
+        render json: user
+    end
+  end
 
+  private 
+
+  def user_params 
+    params.permit(:username, :password, :password_confirmation)
   end
 
 end
